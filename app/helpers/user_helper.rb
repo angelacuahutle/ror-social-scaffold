7994aa1a836a04de957d639829a9e36a3e25903e
angelacuahutle <angela.cuahutle@gmail.com>
2021-08-19 16:30:34 -0500

    Check final version of user helper and model

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,15 +1,16 @@
 module UserHelper
   # Users who have yet to confirme friend requests
   def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact  
-  end 
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  end
+
   def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship = inverse_friendships.find { |friendship| friendship.user == user }
     friendship.confirmed = true
     friendship.save
   end
