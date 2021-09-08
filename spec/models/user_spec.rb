928c42ac462461db8f628574effa78ff4aedd048
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-07 19:09:31 -0500

    add associations and validations to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe User, type: :model do
   let(:user) { User.create(name: 'Angela', email: 'angela@pachis.com', password: 'admin123') }
   describe 'Validations' do
     it { should validate_presence_of(:name) }
-
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_length_of({ maximum: 20 }) }
+    it { should have_many(:friendships) }
+    it { should have_many(:inverse_friendships) }
   end
 end
