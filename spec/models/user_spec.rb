f006316726ebd7df228e9f0c45768ea5b6219e51
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-07 19:08:57 -0500

    add presence of validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create(name: 'Angela', email: 'angela@pachis.com', password: 'admin123') }
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
 
+  end
 end
