df173cd75f693f5787988cb4ed5cf1e2dde1e180
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-15 15:51:18 -0500

    add validator

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,9 +1,21 @@
 class Friendship < ApplicationRecord
+include ActiveModel::Validations
+# validates_with DupplicateValidator, on: :create
+  validates_with SelfFriendshipValidator
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
   def accept_request
     update_attributes(confirmed: true)
+    Friendship.create!(friend_id: user_id,
+                       user_id: friend_id,
+                       confirmed: true)
+  end
+  validates :user, presence: true
+  validates :friend, presence: true
+
+  def confirm_friend
+    # self.update_attributes(confirmed: true)
     Friendship.create!(friend_id: user_id,
                        user_id: friend_id,
                        confirmed: true)
