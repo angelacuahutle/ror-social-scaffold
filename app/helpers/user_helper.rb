35286741ad475a94f361f7e6a981824d381c0989
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-21 14:36:52 -0500

    Friend request is sent and render correct messages accodingly

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -15,17 +15,21 @@ module UserHelper
       render partial: 'user', locals: { user: user }
     elsif current_user.pending_friends.include?(user)
       render partial: 'friendships/confirm_decline', locals: { request: request }
-    elsif current_user.friend_requests.include?(user)
-      render partial: 'friendships/confirm_decline', locals: { request: request }
+    # elsif current_user.friend_requests.include?(user)
+    #   render partial: 'friendships/confirm_decline', locals: { request: request }
     else
       render partial: 'user', locals: { user: user }
     end
   end
 
   def friends_message(user)
+    # byebug
     if current_user.friend?(user)
       content_tag :p, 'You are friends!'
+    elsif current_user.friend_requests.include?(user)
+      content_tag :p, 'You already sent the request.'
     else
+      #byebug
       form_for(current_user.friendships.new, url: user_friendships_path(user)) do |form|
         form.submit 'Add Friend', class: 'btn btn-secondary'
       end
