69fc033b1d11384d495fbd47908a03b8cafb2445
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-22 12:05:20 -0500

    Fix requirements from tse

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,10 +1,8 @@
 module UserHelper
-  # Not in use now
   def current_user?(user)
     current_user == user
   end
 
-  ## conditional rendering
   def friend_request(user)
     render partial: 'friend', locals: { user: user } if current_user.friendship_created?(user) && !current_user?(user)
   end
@@ -13,23 +11,21 @@ module UserHelper
     request = Friendship.all.double_search_requested(current_user, user)
     if current_user.friends.include?(user)
       render partial: 'user', locals: { user: user }
-    elsif current_user.pending_friends.include?(user)
+    elsif current_user.friend_requests.include?(user)
       render partial: 'friendships/confirm_decline', locals: { request: request }
-    # elsif current_user.friend_requests.include?(user)
-    #   render partial: 'friendships/confirm_decline', locals: { request: request }
     else
       render partial: 'user', locals: { user: user }
     end
   end
 
   def friends_message(user)
-    # byebug
-    if current_user.friend?(user)
+    if current_user?(user)
+      content_tag :p, 'Check your profile!'
+    elsif current_user.friend?(user)
       content_tag :p, 'You are friends!'
-    elsif current_user.friend_requests.include?(user)
+    elsif current_user.pending_friends.include?(user)
       content_tag :p, 'You already sent the request.'
     else
-      # byebug
       form_for(current_user.friendships.new, url: user_friendships_path(user)) do |form|
         form.submit 'Add Friend', class: 'btn btn-secondary'
       end
