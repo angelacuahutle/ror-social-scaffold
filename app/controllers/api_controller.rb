7cceaca69543c2a27e44a06b03d5cf79bb0e3220
angelacuahutle <angela.cuahutle@gmail.com>
2022-03-10 15:13:34 -0600

    Fix linters

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,9 @@
 class ApiController < ActionController::API
+  def show_posts
+    render :json => Post.all
+  end
 
-    def show_posts
-        render :json => Post.all
-    end
-
-    def show_comments
-        render :json => Comment.where(post_id: params[:post_id])
-    end
+  def show_comments
+    render :json => Comment.where(post_id: params[:post_id])
+  end
 end
-  
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-    factory :post do
-      user_id { Faker::Number.number(10) }
-      content { Faker::Lorem.word }
-    end
+  factory :post do
+    user_id { Faker::Number.number(10) }
+    content { Faker::Lorem.word }
+  end
 end
