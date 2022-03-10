7f456314a635d37986d9275f6c8761b955dff27a
Jonathas Tavares <jonathastavares@id.uff.br>
2022-03-10 17:42:33 -0300

    Create endpoint for new comment

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,5 +7,15 @@ class ApiController < ActionController::API
     def show_comments
         render :json => Comment.where(post_id: params[:post_id])
     end
+
+    def add_comment
+        @comment = Comment.create(post_id: params[:post_id], user_id: params[:user_id], content: params[:content])
+
+        if @comment.save
+            render :json => "Comment created successfully!"
+        else
+            render :json => "Comment not created!"
+        end
+    end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   get '/api/posts', to: 'api#show_posts'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/api/posts/:post_id/comments', to: 'api#show_comments'
+  post '/api/posts/comment', to: 'api#add_comment'
 end
